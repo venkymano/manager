66eab0956ddb4919d60a7d25001e91771917f95a
vmangalr <vmangalr@akamai.com>
2024-08-29 10:46:55 +0530

    upcoming: [DI-20357] - Enable statuc URL

--- packages/api-v4/src/cloudpulse/dashboards.ts
+++ packages/api-v4/src/cloudpulse/dashboards.ts
@@ -1,21 +1,30 @@
 import { ResourcePage } from 'src/types';
-import Request, { setMethod, setURL } from '../request';
+import Request, { setHeaders, setMethod, setURL } from '../request';
 import { Dashboard } from './types';
-import { API_ROOT } from 'src/constants';
 
 // Returns the list of all the dashboards available
 export const getDashboards = (serviceType: string) =>
   Request<ResourcePage<Dashboard>>(
     setURL(
-      `${API_ROOT}/monitor/services/${encodeURIComponent(
+      `https://blr-lhv95n.bangalore.corp.akamai.com:9000/v4beta/monitor/services/${encodeURIComponent(
         serviceType
       )}/dashboards`
     ),
-    setMethod('GET')
+    setMethod('GET'),
+    setHeaders({
+      Authorization: 'Bearer vagrant',
+    })
   );
 
 export const getDashboardById = (dashboardId: number) =>
   Request<Dashboard>(
-    setURL(`${API_ROOT}/monitor/dashboards/${encodeURIComponent(dashboardId)}`),
-    setMethod('GET')
+    setURL(
+      `https://blr-lhv95n.bangalore.corp.akamai.com:9000/v4beta/monitor/dashboards/${encodeURIComponent(
+        dashboardId!
+      )}`
+    ),
+    setMethod('GET'),
+    setHeaders({
+      Authorization: 'Bearer vagrant',
+    })
   );
