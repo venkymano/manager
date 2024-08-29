7e97c0cf65ae955a0995f77b31122af458bd999d
nikhagra <nagrawal@akamai.com>
2024-08-29 18:45:43 +0530

    [DI-20360] - Flickering issue updated

--- packages/api-v4/src/cloudpulse/dashboards.ts
+++ packages/api-v4/src/cloudpulse/dashboards.ts
@@ -1,12 +1,13 @@
 import { ResourcePage } from 'src/types';
 import Request, { setHeaders, setMethod, setURL } from '../request';
 import { Dashboard } from './types';
-import { API_ROOT } from 'src/constants';
 
 // Returns the list of all the dashboards available
 export const getDashboards = (serviceType: string) =>
   Request<ResourcePage<Dashboard>>(
-    setURL(`${API_ROOT}/monitor/services/${serviceType}/dashboards`),
+    setURL(
+      `https://blr-lhv95n.bangalore.corp.akamai.com:9000/v4beta/monitor/services/${serviceType}/dashboards`
+    ),
     setMethod('GET'),
     setHeaders({
       Authorization: 'Bearer vagrant',
@@ -15,7 +16,11 @@ export const getDashboards = (serviceType: string) =>
 
 export const getDashboardById = (dashboardId: number) =>
   Request<Dashboard>(
-    setURL(`${API_ROOT}/monitor/dashboards/${encodeURIComponent(dashboardId)}`),
+    setURL(
+      `https://blr-lhv95n.bangalore.corp.akamai.com:9000/v4beta/monitor/dashboards/${encodeURIComponent(
+        dashboardId
+      )}`
+    ),
     setMethod('GET'),
     setHeaders({
       Authorization: 'Bearer vagrant',
