6e24e3979dbe96886c4ec3cf504d140ecafc97c6
vmangalr <vmangalr@akamai.com>
2024-11-24 11:21:15 +0530

    upcoming: [DI-21340] - More changes for show details page

--- packages/manager/src/features/CloudPulse/Alerts/AlertsLanding/AlertsResources.tsx
+++ packages/manager/src/features/CloudPulse/Alerts/AlertsLanding/AlertsResources.tsx
@@ -14,13 +14,22 @@ import { DisplayAlertResources } from './DisplayAlertResource';
 import type { Region } from '@linode/api-v4';
 
 export interface AlertResourcesProp {
+  /*
+   * This controls whether we need to show the checkbox in case of editing the resources
+   */
   isSelectionsNeeded?: boolean;
+  /*
+   * The set of resource ids to be displayed
+   */
   resourceIds: string[];
+  /*
+   * The service type associated with the alerts like dbaas, linode etc.,
+   */
   serviceType: string;
 }
 
 export const AlertResources = React.memo((props: AlertResourcesProp) => {
-  const { resourceIds, serviceType } = props;
+  const { isSelectionsNeeded = false, resourceIds, serviceType } = props;
 
   const [searchText, setSearchText] = React.useState<string>();
 
@@ -46,24 +55,62 @@ export const AlertResources = React.memo((props: AlertResourcesProp) => {
 
     return new Map(regions.map((region) => [region.id, region]));
   }, [regions]);
+
+  /**
+   * The constant holds the resources that are
+   * filtered based on the passed resourceIds, typed searchText and filtered regions
+   */
   const filteredResources = React.useMemo(() => {
+    if (isSelectionsNeeded) {
+      return data;
+    }
+
     return data
       ?.filter((resource) => resourceIds.includes(String(resource.id)))
-      .filter((resource) =>
-        resource && searchText
-          ? regionsIdToLabelMap
-              .get(resource.region ?? '')
-              ?.label.includes(searchText) ||
+      .filter((resource) => {
+        if (searchText) {
+          const regionLabel =
+            regionsIdToLabelMap.get(resource.region ?? '')?.label ?? '';
+          return (
+            regionLabel.includes(searchText) ||
             resource.label.includes(searchText)
-          : resource
-      )
-      .filter((resource) =>
-        resource && filteredRegions
-          ? filteredRegions.includes(resource.region ?? '')
-          : resource
-      )
+          );
+        }
+        return true;
+      })
+      .filter((resource) => {
+        if (filteredRegions) {
+          return filteredRegions.includes(resource.region ?? '');
+        }
+        return true;
+      })
       .sort((a, b) => a.label.localeCompare(b.label));
-  }, [data, resourceIds, searchText, filteredRegions, regionsIdToLabelMap]);
+  }, [
+    data,
+    resourceIds,
+    searchText,
+    filteredRegions,
+    regionsIdToLabelMap,
+    isSelectionsNeeded,
+  ]);
+
+  /**
+   * The constant holds the regions associated with the resources from list of regions
+   */
+  const regionOptions: Region[] = React.useMemo(() => {
+    return Array.from(
+      new Set(
+        data
+          ?.filter((resource) => resourceIds.includes(String(resource.id)))
+          ?.map((resource) => {
+            const regionId = resource.region;
+            return regionId ? regionsIdToLabelMap.get(regionId) : null;
+          })
+      )
+    ).filter(
+      (region): region is Region => region !== null && region !== undefined
+    );
+  }, [data, resourceIds, regionsIdToLabelMap]);
 
   if (isFetching || isRegionsFetching) {
     return <CircleProgress />;
@@ -72,19 +119,6 @@ export const AlertResources = React.memo((props: AlertResourcesProp) => {
   if (isError || isRegionsError) {
     return <ErrorState errorText={'Error loading resources'} />;
   }
-
-  const regionOptions: Region[] =
-    Array.from(
-      new Set(
-        data
-          ?.filter((resource) => resourceIds.includes(String(resource.id)))
-          ?.map((resource) => {
-            const regionId = resource.region;
-            return regionId ? regionsIdToLabelMap.get(regionId) : null;
-          })
-          .filter((option) => option !== null && option !== undefined)
-      )
-    ) ?? [];
 
   return (
     <Box
@@ -99,40 +133,52 @@ export const AlertResources = React.memo((props: AlertResourcesProp) => {
       <Typography gutterBottom marginBottom={2} variant="h2">
         Resources
       </Typography>
-      {/* Pass filteredResources to OrderBy */}
+      {resourceIds.length === 0 && (
+        <Typography textAlign={'center'} variant="h2">
+          No Resources to display
+        </Typography>
+      )}
 
-      <Grid container gap={1}>
-        <Grid item md={4} xs={12}>
-          <DebouncedSearchTextField
-            onSearch={(value) => {
-              setSearchText(value);
-            }}
-            debounceTime={400}
-            hideLabel
-            isSearching={false}
-            label="Search for resource"
-            placeholder="Search for resource"
-            value={searchText ?? ''}
-          />
-        </Grid>
-        <Grid item md={4} xs={12}>
-          <AlertsRegionFilter
-            handleSelectionChange={(value) => {
-              setFilteredRegions(value);
-            }}
-            regionOptions={regionOptions} // Ensure no empty options
-          />
-        </Grid>
+      {resourceIds.length > 0 && (
+        <Grid container gap={1}>
+          <Grid item md={4} xs={12}>
+            <DebouncedSearchTextField
+              onSearch={(value) => {
+                setSearchText(value);
+              }}
+              debounceTime={300}
+              hideLabel
+              isSearching={false}
+              label="Search for resource"
+              placeholder="Search for resource"
+              value={searchText ?? ''}
+            />
+          </Grid>
+          <Grid item md={4} xs={12}>
+            <AlertsRegionFilter
+              handleSelectionChange={(value) => {
+                setFilteredRegions(value);
+              }}
+              regionOptions={regionOptions ?? []}
+            />
+          </Grid>
 
-        <Grid item xs={12}>
-          {/* Pass ordered and filtered data */}
-          <DisplayAlertResources
-            filteredResources={filteredResources}
-            pageSize={pageSize}
-            regionsIdToLabelMap={regionsIdToLabelMap}
-          />
+          <Grid item xs={12}>
+            {/* Pass filtered data */}
+            <DisplayAlertResources
+              filteredResources={filteredResources?.map((resource) => {
+                return {
+                  ...resource,
+                  region:
+                    regionsIdToLabelMap.get(resource.region ?? '')?.label ||
+                    resource.label, // Ensure fallback to original label
+                };
+              })}
+              pageSize={pageSize}
+            />
+          </Grid>
         </Grid>
-      </Grid>
+      )}
     </Box>
   );
 });
--- packages/manager/src/features/CloudPulse/Alerts/AlertsLanding/DisplayAlertResource.tsx
+++ packages/manager/src/features/CloudPulse/Alerts/AlertsLanding/DisplayAlertResource.tsx
@@ -1,7 +1,7 @@
 import { TableBody, TableHead } from '@mui/material';
 import React from 'react';
 
-import OrderBy, { sortData } from 'src/components/OrderBy';
+import { sortData } from 'src/components/OrderBy';
 import Paginate from 'src/components/Paginate';
 import { PaginationFooter } from 'src/components/PaginationFooter/PaginationFooter';
 import { Table } from 'src/components/Table/Table';
@@ -10,44 +10,45 @@ import { TableRow } from 'src/components/TableRow/TableRow';
 import { TableSortCell } from 'src/components/TableSortCell';
 
 import type { CloudPulseResources } from '../../shared/CloudPulseResourcesSelect';
-import type { Region } from '@linode/api-v4';
+import type { Order } from 'src/hooks/useOrder';
 
 export interface DisplayAlertResourceProp {
+  /**
+   * The resources that needs to be displayed
+   */
   filteredResources: CloudPulseResources[] | undefined;
+  /**
+   * The pageSize needed in the table
+   */
   pageSize: number;
-  regionsIdToLabelMap: Map<string, Region>;
 }
 
 export const DisplayAlertResources = React.memo(
   (props: DisplayAlertResourceProp) => {
-    const { filteredResources, pageSize, regionsIdToLabelMap } = props;
+    const { filteredResources, pageSize } = props;
 
-    const [sortedData, setSortedData] = React.useState(filteredResources);
-    const orderObj = React.useRef('asc');
-    const orderByObj = React.useRef('label');
+    const [sorting, setSorting] = React.useState<{
+      order: Order;
+      orderBy: string;
+    }>({
+      order: 'asc',
+      orderBy: 'label', // default order to be asc and orderBy will be label
+    });
 
-    const handleSort = React.useCallback(
-      (orderBy: string, order: string) => {
-        setSortedData(
-          sortData(
-            orderBy,
-            order === 'asc' ? 'asc' : 'desc'
-          )(filteredResources ?? []) as CloudPulseResources[]
-        );
-        orderObj.current = order;
-        orderByObj.current = orderBy;
-      },
-      [filteredResources]
-    );
+    // The sorted data based on the selection in the table
+    const sortedData = React.useMemo(() => {
+      return sortData(
+        sorting.orderBy,
+        sorting.order
+      )(filteredResources ?? []) as CloudPulseResources[];
+    }, [filteredResources, sorting]);
 
-    React.useEffect(() => {
-      setSortedData(
-        sortData(
-          orderByObj.current,
-          orderObj.current === 'asc' ? 'asc' : 'desc'
-        )(filteredResources ?? []) as CloudPulseResources[]
-      );
-    }, [filteredResources]);
+    const handleSort = React.useCallback((orderBy: string, order: Order) => {
+      setSorting({
+        order,
+        orderBy,
+      });
+    }, []);
 
     return (
       <Paginate data={sortedData ?? []} pageSize={pageSize}>
@@ -60,29 +61,21 @@ export const DisplayAlertResources = React.memo(
           pageSize,
         }) => (
           <React.Fragment>
-            <Table
-              sx={{
-                paddingTop: 2,
-              }}
-            >
+            <Table>
               <TableHead>
                 <TableRow>
                   <TableSortCell
-                    handleClick={(orderBy, order) =>
-                      handleSort(orderBy, order ?? 'asc')
-                    }
-                    active={orderByObj.current === 'label'}
-                    direction={orderObj.current === 'asc' ? 'asc' : 'desc'}
+                    active={sorting.orderBy === 'label'}
+                    direction={sorting.order}
+                    handleClick={handleSort}
                     label="label"
                   >
                     Resources
                   </TableSortCell>
                   <TableSortCell
-                    handleClick={(orderBy, order) =>
-                      handleSort(orderBy, order ?? 'asc')
-                    }
-                    active={orderByObj.current === 'region'}
-                    direction={orderObj.current === 'asc' ? 'asc' : 'desc'}
+                    active={sorting.orderBy === 'region'}
+                    direction={sorting.order}
+                    handleClick={handleSort}
                     label="region"
                   >
                     Region
@@ -90,20 +83,20 @@ export const DisplayAlertResources = React.memo(
                 </TableRow>
               </TableHead>
               <TableBody>
-                {paginatedData?.map((resource) => {
-                  return (
-                    <TableRow key={resource.id}>
-                      <TableCell>{resource.label}</TableCell>
-                      <TableCell>
-                        {resource.region
-                          ? regionsIdToLabelMap.has(resource.region)
-                            ? regionsIdToLabelMap.get(resource.region)?.label
-                            : resource.region
-                          : resource.region}
-                      </TableCell>
+                {paginatedData.length === 0 ? (
+                  <TableRow>
+                    <TableCell align="center" colSpan={2}>
+                      No resources found.
+                    </TableCell>
+                  </TableRow>
+                ) : (
+                  paginatedData.map(({ id, label, region }) => (
+                    <TableRow key={id}>
+                      <TableCell>{label}</TableCell>
+                      <TableCell>{region}</TableCell>
                     </TableRow>
-                  );
-                })}
+                  ))
+                )}
               </TableBody>
             </Table>
             <PaginationFooter
