2df4c0f9b1a5e4ef99a170f75960252788731401
vmangalr <vmangalr@akamai.com>
2024-12-02 16:03:05 +0530

    upcoming: [DI-21340] - UT fix

--- packages/manager/src/features/CloudPulse/Alerts/AlertsDetail/AlertDetailOverview.test.tsx
+++ packages/manager/src/features/CloudPulse/Alerts/AlertsDetail/AlertDetailOverview.test.tsx
@@ -1,24 +1,51 @@
 import React from 'react';
 
-import { alertFactory } from 'src/factories';
+import { alertFactory, serviceTypesFactory } from 'src/factories';
 import { renderWithTheme } from 'src/utilities/testHelpers';
 
+import { convertStringToCamelCasesWithSpaces } from '../../Utils/utils';
+import { severityMap } from '../constants';
 import { AlertDetailOverview } from './AlertDetailOverview';
+
+// Mock Data
+const serviceTypes = serviceTypesFactory.buildList(1, {
+  label: 'Databases',
+  service_type: 'dbaas',
+});
+// Mock Queries
+const queryMocks = vi.hoisted(() => ({
+  useCloudPulseServiceTypes: vi.fn(),
+}));
+
+vi.mock('src/queries/cloudpulse/services', () => ({
+  ...vi.importActual('src/queries/cloudpulse/services'),
+  useCloudPulseServiceTypes: queryMocks.useCloudPulseServiceTypes,
+}));
+
+queryMocks.useCloudPulseServiceTypes.mockReturnValue({
+  data: { data: serviceTypes },
+  isError: false,
+  isFetching: false,
+});
 
 describe('AlertDetailOverview component tests', () => {
   it('should render alert detail overview with required props', () => {
     const alert = alertFactory.build({
       description: 'This is test description',
-      label: undefined,
+      label: 'Test alert',
       severity: 3,
     });
-    const screen = renderWithTheme(<AlertDetailOverview alert={alert} />);
+    const { getByText } = renderWithTheme(
+      <AlertDetailOverview alert={alert} />
+    );
 
     const { description, label, severity, type } = alert;
 
-    expect(screen.getByText(description)).toBeInTheDocument();
-    expect(screen.getByText(String(severity))).toBeInTheDocument();
-    expect(screen.getByText(label)).toBeInTheDocument();
-    expect(screen.getByText(type)).toBeInTheDocument();
+    expect(getByText(description)).toBeInTheDocument();
+    expect(getByText(String(severityMap[severity]))).toBeInTheDocument();
+    expect(getByText(label)).toBeInTheDocument();
+    expect(
+      getByText(convertStringToCamelCasesWithSpaces(type))
+    ).toBeInTheDocument();
   });
 });
