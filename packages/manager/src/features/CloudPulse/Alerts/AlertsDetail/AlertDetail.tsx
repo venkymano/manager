180dffa60baed0d167a4d2f1f393cc135c6435d0
vmangalr <vmangalr@akamai.com>
2024-12-10 11:23:46 +0530

    upcoming: [DI-21340] - Alert detail proper updates

--- packages/manager/src/features/CloudPulse/Alerts/AlertsDetail/AlertDetail.tsx
+++ packages/manager/src/features/CloudPulse/Alerts/AlertsDetail/AlertDetail.tsx
@@ -1,5 +1,5 @@
 import { Box, Chip, CircleProgress } from '@linode/ui';
-import { Grid, styled, useTheme } from '@mui/material';
+import { Grid, styled, useMediaQuery, useTheme } from '@mui/material';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
@@ -9,16 +9,18 @@ import { ErrorState } from 'src/components/ErrorState/ErrorState';
 import { Placeholder } from 'src/components/Placeholder/Placeholder';
 import { useAlertDefinitionQuery } from 'src/queries/cloudpulse/alerts';
 
-import { getAlertBoxStyles } from '../Utils/utils';
 import { AlertDetailCriteria } from './AlertDetailCriteria';
 import { AlertDetailOverview } from './AlertDetailOverview';
 
+import type { BreadcrumbProps } from 'src/components/Breadcrumb/Breadcrumb';
+import { getAlertBoxStyles } from '../Utils/utils';
+
 interface RouteParams {
-  /**
+  /*
    * The id of the alert for which the data needs to be shown
    */
   alertId: string;
-  /**
+  /*
    * The service type like linode, dbaas etc., of the the alert for which the data needs to be shown
    */
   serviceType: string;
@@ -32,68 +34,76 @@ export const AlertDetail = () => {
     serviceType
   );
 
-  const generateCrumbOverrides = () => {
+  const { crumbOverrides, pathname } = React.useMemo((): BreadcrumbProps => {
     const overrides = [
       {
         label: 'Definitions',
-        linkTo: '/monitor/cloudpulse/alerts/definitions',
+        linkTo: '/monitor/alerts/definitions',
         position: 1,
       },
       {
         label: 'Details',
-        linkTo: `/monitor/cloudpulse/alerts/definitions/details/${serviceType}/${alertId}`,
+        linkTo: `/monitor/alerts/definitions/details/${serviceType}/${alertId}`,
         position: 2,
       },
     ];
-
-    return { newPathname: '/Definitions/Details', overrides };
-  };
-
-  const { newPathname, overrides } = React.useMemo(
-    () => generateCrumbOverrides(),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    []
-  );
+    return { crumbOverrides: overrides, pathname: '/Definitions/Details' };
+  }, [alertId, serviceType]);
 
   const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const flexWrap = isSmallScreen ? 'wrap' : 'nowrap';
 
   if (isFetching) {
     return (
-      <Grid alignItems="center" container height={theme.spacing(75)}>
-        <Grid item xs={12}>
-          <CircleProgress />
+      <>
+        <Breadcrumb crumbOverrides={crumbOverrides} pathname={pathname} />
+        <Grid alignItems={'center'} container height={theme.spacing(75)}>
+          <Grid item xs={12}>
+            <CircleProgress />
+          </Grid>
         </Grid>
-      </Grid>
+      </>
     );
   }
 
   if (isError) {
     return (
-      <Grid alignItems="center" container height={theme.spacing(75)}>
-        <Grid item xs={12}>
-          <ErrorState
-            errorText={
-              'An error occurred while loading the definitions. Please try again later.'
-            }
-          />
+      <>
+        <Breadcrumb crumbOverrides={crumbOverrides} pathname={pathname} />
+        <Grid alignItems={'center'} container height={theme.spacing(75)}>
+          <Grid item xs={12}>
+            <ErrorState
+              errorText={
+                'An error occurred while loading the definitions. Please try again later'
+              }
+            />
+          </Grid>
         </Grid>
-      </Grid>
+      </>
     );
   }
 
   if (!alertDetails) {
     return (
-      <Grid alignItems="center" container height={theme.spacing(75)}>
-        <Grid item xs={12}>
-          <StyledPlaceholder icon={EntityIcon} title="No Data to display." />
+      <>
+        <Breadcrumb crumbOverrides={crumbOverrides} pathname={pathname} />
+        <Grid alignItems={'center'} container height={theme.spacing(75)}>
+          <Grid item xs={12}>
+            <StyledPlaceholder
+              icon={EntityIcon}
+              isEntity
+              title="No Data to display."
+            />
+          </Grid>
         </Grid>
-      </Grid>
+      </>
     );
   }
 
   return (
     <React.Fragment>
-      <Breadcrumb crumbOverrides={overrides} pathname={newPathname} />
+      <Breadcrumb crumbOverrides={crumbOverrides} pathname={pathname} />
       <Box display="flex" flexDirection="column" gap={2}>
         <Box display="flex" flexDirection={{ md: 'row', xs: 'column' }} gap={2}>
           <Box
